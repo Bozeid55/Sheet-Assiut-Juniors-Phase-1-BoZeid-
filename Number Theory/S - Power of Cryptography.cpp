98b882fdbcda6981a394156a534f5bb68480efde
Bozeid55 <157320734+Bozeid55@users.noreply.github.com>
2025-12-10 21:02:54 +0200

    Create S - Power of Cryptography.cpp

---
+++ Number Theory/S - Power of Cryptography.cpp
@@ -0,0 +1,35 @@
+// وَكَانَ فَضْلُ اللَّهِ عَلَيْكَ عَظِيمًا♡
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define all(x) x.begin(), x.end()
+#define input(vec) for (auto& i : vec) cin >> i
+#define inputone(vec , x) for (int i = 1 ; i <= x ; i++) cin >> vec[i]
+#define ABo_ZeiD ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+const ll N = 1e18;
+
+
+void solve() {
+    ld n , p;
+    
+    while (cin >> n >> p) {
+        cout << fixed << setprecision(0) << pow(p , 1 / n) << endl;
+    }
+    
+}
+
+int main() {
+    ABo_ZeiD;
+
+    //freopen("smallest.in" , "r", stdin);
+
+    int t = 1;
+
+    //cin >> t;
+    while (t--)
+        solve();
+
+
+    return 0;
+}
